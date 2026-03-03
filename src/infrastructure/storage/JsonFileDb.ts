934c83aa630f20443d2017ca9a47e5772fc15952
ichoqueru <ichoquer@unjbg.edu.pe>
2026-03-02 21:36:12 -0500

    Modifica JsonFileDb

--- src/infrastructure/storage/JsonFileDb.ts
+++ src/infrastructure/storage/JsonFileDb.ts
@@ -4,12 +4,17 @@ export class JsonFileDb<T> {
   constructor(private path: string) {}
 
   private async asegurarArchivo(): Promise<void> {
-    try {
-      await fs.promises.access(this.path);
-    } catch {
-      await fs.promises.writeFile(this.path, JSON.stringify([]), { encoding: "utf-8" });
-    }
+  // Crear carpeta si no existe
+  const carpeta = this.path.substring(0, this.path.lastIndexOf("/"));
+  if (carpeta) {
+    await fs.promises.mkdir(carpeta, { recursive: true });
   }
+  try {
+    await fs.promises.access(this.path);
+  } catch {
+    await fs.promises.writeFile(this.path, JSON.stringify([]), { encoding: "utf-8" });
+  }
+}
 
   async leerTodo(): Promise<T[]> {
     await this.asegurarArchivo();
